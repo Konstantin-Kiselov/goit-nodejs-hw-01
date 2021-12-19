1af2e2ea898457d4c860cb78e022bf6c5a666f02
Konstantin Kiselov <constantine.kiss@gmail.com>
2021-12-20 01:34:57 +0200

    add yargs and fixed contactsOperation

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const contactsOperations = require('./contacts');
+const argv = require('yargs').argv;
 
-const invokeAction = async (action, filePath, data) => {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case 'getAll':
-      const contacts = await contactsOperations.getAll();
+    case 'list':
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contact = await contactsOperations.getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'add':
+      const newContact = await contactsOperations.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const deleteContact = await contactsOperations.removeContact(id);
+      console.log(deleteContact);
       break;
 
     default:
-      console.log('Unknown action');
+      console.warn('\x1B[31m Unknown action type!');
       break;
   }
 };
+
+invokeAction(argv);
